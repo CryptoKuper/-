e04e9e9f42c962010e14e3eb2c9e5e9b672d0de6
Rafael <r.tukanov1981@gmail.com>
2023-12-03 11:43:43 +0600

    Добавил решение

---
+++ Задача 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив из 10 целых чисел
+        int[] numbers = new int[] { 2, 7, 14, 19, 26, 33, 40, 55, 68, 77 };
+
+        // Находим количество чётных чисел в массиве
+        int countEven = CountEvenNumbers(numbers);
+
+        // Выводим результат
+        Console.WriteLine($"Количество чётных чисел в массиве: {countEven}");
+    }
+
+    // Метод для подсчета чётных чисел в массиве
+    static int CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+
+        foreach (var number in array)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
